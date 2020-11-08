f96c3b749bea83a2dd25bc3f20b4b00f5d78be36
Sergey Ugryumov <snugryumov@gmail.com>
2020-11-08 18:42:59 +0300

    two sum task

---
+++ twoSum/index.test.js
@@ -0,0 +1,13 @@
+const twoSum = require(".");
+
+test("test [0, 1]", () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+
+test("test [1, 2]", () => {
+  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+});
+
+test("test [0, 2]", () => {
+  expect(twoSum([1, 2, 3], 4)).toEqual([0, 2]);
+});
